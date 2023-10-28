dd25407f8505e21ef4eb55f536968bb4433a15e1
xincheng <xz353@duke.edu>
2023-10-28 14:37:16 -0400

    Initial Commit

---
+++ DukeEventCalendar/DukeEventCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DukeEventCalendarApp.swift
+//  DukeEventCalendar
+//
+//  Created by xz353 on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DukeEventCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
